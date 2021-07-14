2038a8209f9b23b2a549e02dc5e65bf35b8e8fd7
Kagen Hearn <kl.hearn40@gmail.com>
2021-07-14 19:29:05 -0400

    First functioning error handlers

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const csrf = require("csurf");
 const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
+const { ValidationError } = require("sequelize");
 
 const routes = require("./routes");
 
@@ -37,5 +38,35 @@ app.use(
 
 // Application routers
 app.use(routes);
+
+app.use((req, res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    if(err instanceof ValidationError) {
+        err.errors = err.errors.map((e) => e.message);
+        err.title = 'Validation error';
+    }
+
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack
+    });
+});
+
+
 
 module.exports = app;
