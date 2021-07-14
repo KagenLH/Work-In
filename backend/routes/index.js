59016ef30a314e0d5def0fa5e3c1d9f7de96756b
Kagen Hearn <kl.hearn40@gmail.com>
2021-07-14 19:20:36 -0400

    Got first API route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,10 @@
 const express = require("express");
+
+const apiRouter = require('./api');
+
 const router = express.Router();
+
+router.use('/api', apiRouter);
 
 router.get('/hello/world', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
