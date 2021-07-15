33aa03cd632ea99c1868b9f5270e33fe2b7e7072
Kagen Hearn <kl.hearn40@gmail.com>
2021-07-15 15:04:02 -0400

    Added CSRF token functionality to the frontend in both production and development environments

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,10 +6,14 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import configureStore from './store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 
 const store = configureStore();
 
-if(process.env.NODE_ENV !== production) {
+if(process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
 
---
+++ frontend/src/store/csrf.js
@@ -0,0 +1,21 @@
+import Cookies from 'js-cookie';
+
+export async function csrfFetch(url, options = {}) {
+    options.method = options.method || 'GET';
+    options.headers = options.headers || {};
+
+    if(options.method.toUpperCase() !== 'GET') {
+        options.headers['Content-Type'] = options.headers['Content-Type'] || 'application/json';
+        options.headers['XSRF-Token'] = Cookies.get('XSRF-TOKEN');
+    }
+
+    const res = await window.fetch(url, options);
+
+    if(res.status >= 400) throw res;
+
+    return res;
+}
+
+export function restoreCSRF() {
+    return csrfFetch('/api/csrf/restore');
+}
